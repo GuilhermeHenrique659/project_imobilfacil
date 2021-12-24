fe1f9807502948665fb98c0fd64f9a2652270125
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2021-12-24 16:57:16 -0300

    update dashboard menu

--- config.py
+++
@@ -1,9 +0,0 @@
-from main import db
-from dao import imovelDao, cad_proprietario_dao, cad_corretor_dao,tiposDao,ciadadeDao,bairroDao,financeiroDao
-
-#banco para teste
-
-
-
-
-FinDao = financeiroDao(db)
